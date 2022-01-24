9a23b22618147123b8544451c381ce9f4d61923f
kartikchandra <kartik.chandra@mobilecoderz.com>
2022-01-24 23:35:41 +0530

    code initiate

---
+++ frontend/src/app/pages/starter/starter.component.ts
@@ -0,0 +1,22 @@
+
+/*
+ * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Licensed under the Single Application / Multi Application License.
+ * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'ngx-starter-menu',
+    styleUrls: ['./starter.component.scss'],
+    templateUrl: './starter.component.html',
+  })
+  export class StarterMenuComponent implements OnInit {
+    constructor() {}
+
+ngOnInit(): void {
+}
+
+  }
